6a5982377316143d15e43a780f277e5dda9eb310
Leigham MAsters <itsleigham@gmail.com>
2024-07-24 03:26:53 +0100

    added public folder support

--- src/services/Server.ts
+++ src/services/Server.ts
@@ -9,12 +9,20 @@ export class Server {
   private host: string;
   private port: number;
   private routesPath: string;
+  private enablePublicRoutes: boolean;
 
-  constructor(https: boolean, host: string, port: number, routesPath: string) {
+  constructor(
+    https: boolean,
+    host: string,
+    port: number,
+    routesPath: string,
+    enablePublicRoutes: boolean
+  ) {
     this.https = https;
     this.host = host;
     this.port = port;
     this.routesPath = routesPath;
+    this.enablePublicRoutes = enablePublicRoutes;
   }
 
   public start(): void {
@@ -26,6 +34,18 @@ export class Server {
 
     const app = express();
 
+    if (this.enablePublicRoutes) {
+      // lets see if the public folder exsits
+      if (!fs.existsSync(path.join(__dirname, "../public"))) {
+        fs.mkdirSync(path.join(__dirname, "../public"), { recursive: true });
+        // add a basic file for testing
+        fs.writeFileSync(
+          path.join(__dirname, "../public/index.html"),
+          "<h1>Hello, World!</h1>"
+        );
+      }
+      app.use(express.static(path.join(__dirname, "../public")));
+    }
     // Load routes
     const routeLoader = new RouteLoader(this.routesPath, app);
     routeLoader.loadRoutes();
@@ -53,5 +73,5 @@ export class Server {
 const routesPath = path.join(__dirname, "../routes"); // Adjust as necessary
 
 // Create and start the server
-const server = new Server(false, "localhost", 4000, routesPath);
+const server = new Server(false, "localhost", 4000, routesPath, true);
 server.start();
